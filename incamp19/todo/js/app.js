090609039e8678d88aff9fd74ec6d4b994bc74ad
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-11 16:38:58 +0200

    Adapt layout for task structure

--- incamp19/todo/js/app.js
+++ incamp19/todo/js/app.js
@@ -24,8 +24,10 @@ function taskToDom(task) {
     html.push(`<div class='task-item'>`);
 
     html.push(`<div class='task-item__header'>`);
+    html.push(`<div class='task-item__title-container'>`);
     html.push(`<input type="checkbox" class="task-item__doneCheckbox" `, task.done ? `checked` : ``, `>`);
     html.push(`<h3 class="task-item__name `, task.done ? `task-item__name_done` : ``, `">`, task.name, "</h3>");
+    html.push(`</div>`); // title-container
     if (task.dueDateTime) {
         html.push(`<div class="task-item__dueDateTime `, isTaskOutdated(task) ? `task-item__dueDateTime_outdated` : ``, `">`, getTaskDueDateTime(task), "</div>");
     }
@@ -44,7 +46,7 @@ let tasks = [
     new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
     new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
     new Task("Task Without Date", true, null, null),
-    new Task("Task Outdated", true, null, new Date("12.05.2015 14:00")),
+    new Task("Task Outdated", true, "This task is outdated...", new Date("12.05.2015 14:00")),
 ];
 
 let taskListDom = document.getElementById("task-list");
