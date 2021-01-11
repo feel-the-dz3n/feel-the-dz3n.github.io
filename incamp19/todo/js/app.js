4ff4b91871a334b6f345384577019cc47fcc8c41
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-11 16:28:40 +0200

    Implement adding tasks from JS

--- incamp19/todo/js/app.js
+++ incamp19/todo/js/app.js
@@ -1,27 +1,54 @@
-var installBtn = document.getElementById('install-btn');
-
-if(installBtn) {
-    
-    installBtn.style.display = 'none';
-    
-    // If you want an installation button, add this to your HTML:
-    //
-    // <button id="install-btn">Install</button>
-    //
-    // This code shows the button if the apps platform is available
-    // and this app isn't already installed.
-    if(navigator.mozApps) {
-
-        installBtn.addEventListener('click', function() {
-            navigator.mozApps.install(location.href + 'manifest.webapp');
-        }, false);
-
-        var req = navigator.mozApps.getSelf();
-        req.onsuccess = function() {
-            if(!req.result) {
-                installBtn.style.display = 'block';
-            }
-        };
-
+class Task {
+    constructor(name, done, description, dueDateTime) {
+        this.name = name;
+        this.done = done;
+        this.description = description;
+        this.dueDateTime = dueDateTime;
     }
+}
+
+function isTaskOutdated(task) {
+    let now = new Date();
+    return now > task.dueDateTime;
+}
+
+function getTaskDueDateTime(task) {
+    let date = task.dueDateTime;
+    // FIXME: leading zeros
+    return `${date.getDay()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+}
+
+function taskToDom(task) {
+    var html = [];
+
+    html.push(`<div class='task-item'>`);
+
+    html.push(`<div class='task-item__header'>`);
+    html.push(`<input type="checkbox" class="task-item__doneCheckbox" `, task.done ? `checked` : ``, `>`);
+    html.push(`<h3 class="task-item__name `, task.done ? `task-item__name_done` : ``, `">`, task.name, "</h3>");
+    if (task.dueDateTime) {
+        html.push(`<div class="task-item__dueDateTime `, isTaskOutdated(task) ? `task-item__dueDateTime_outdated` : ``, `">`, getTaskDueDateTime(task), "</div>");
+    }
+    html.push(`</div>`);
+
+    if (task.description) {
+        html.push(`<p class="task-item__description">`, task.description, `</p>`);
+    }
+
+    html.push(`</div>`);
+
+    return html.join("");
+}
+
+let tasks = [
+    new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
+    new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
+    new Task("Task Without Date", true, null, null),
+    new Task("Task Outdated", true, null, new Date("12.05.2015 14:00")),
+];
+
+let taskListDom = document.getElementById("task-list");
+
+for (let task of tasks) {
+    taskListDom.innerHTML += taskToDom(task);
 }
