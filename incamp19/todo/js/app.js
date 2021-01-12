668f2c0085d052f01c791d93f4089d9739e1bab2
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-12 10:26:53 +0200

    Filter layout

--- incamp19/todo/js/app.js
+++ incamp19/todo/js/app.js
@@ -11,18 +11,18 @@ let tasks = [
     new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
     new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
     new Task("Task Without Date", true, null, null),
-    new Task("Task Outdated", true, "This task is outdated...", new Date("12.05.2015 14:00")),
+    new Task("Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
     new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
     new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
     new Task("Task Without Date", true, null, null),
-    new Task("Task Outdated", true, "This task is outdated...", new Date("12.05.2015 14:00")),
+    new Task("Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
     new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
     new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
     new Task("Task Without Date", true, null, null),
-    new Task("Task Outdated", true, "This task is outdated...", new Date("12.05.2015 14:00")),
+    new Task("Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
 ];
 
-function isTaskOutdated(task) {
+function isTaskOverdue(task) {
     let now = new Date();
     return now > task.dueDateTime;
 }
@@ -39,13 +39,15 @@ function taskToDom(task) {
     html.push(`<div class='task-item'>`);
 
     html.push(`<div class='task-item__header'>`);
-    html.push(`<div class='task-item__title-container'>`);
+    html.push(`<div class='task-item__title-side-container'>`);
     html.push(`<input type="checkbox" class="task-item__doneCheckbox" `, task.done ? `checked` : ``, `>`);
     html.push(`<h3 class="task-item__name `, task.done ? `task-item__name_done` : ``, `">`, task.name, "</h3>");
-    html.push(`</div>`); // task-item__title-container
-    if (task.dueDateTime) {
-        html.push(`<div class="task-item__dueDateTime `, isTaskOutdated(task) ? `task-item__dueDateTime_outdated` : ``, `">`, getTaskDueDateTime(task), "</div>");
+    html.push(`</div>`); // task-item__title-side-container
+    html.push(`<div class='task-item__title-side-container'>`);
+    if (task.dueDateTime && !task.done) {
+        html.push(`<div class="task-item__dueDateTime `, isTaskOverdue(task) ? `task-item__dueDateTime_overdue` : ``, `">`, getTaskDueDateTime(task), "</div>");
     }
+    html.push(`</div>`); // task-item__title-side-container
     html.push(`</div>`); // task-item__header
 
     if (task.description) {
