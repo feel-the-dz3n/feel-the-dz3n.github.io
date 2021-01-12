2ec1d85a4829ce9c93f1490cbae2d43e9c7ac917
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-12 11:07:17 +0200

    Implement filtering

--- incamp19/todo/js/app.js
+++ incamp19/todo/js/app.js
@@ -1,5 +1,6 @@
 class Task {
-    constructor(name, done, description, dueDateTime) {
+    constructor(id, name, done, description, dueDateTime) {
+        this.id = id;
         this.name = name;
         this.done = done;
         this.description = description;
@@ -8,18 +9,18 @@ class Task {
 }
 
 let tasks = [
-    new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
-    new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
-    new Task("Task Without Date", true, null, null),
-    new Task("Task Overdue", false, "This task is overdue...", new Date("12.05.2015 14:00")),
-    new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
-    new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
-    new Task("Task Without Date", true, null, null),
-    new Task("Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
-    new Task("Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
-    new Task("Task Two", true, null, new Date("12.05.2021 14:00")),
-    new Task("Task Without Date", true, null, null),
-    new Task("Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
+    new Task(1, "Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
+    new Task(2, "Task Two", true, null, new Date("12.05.2021 14:00")),
+    new Task(3, "Task Without Date", true, null, null),
+    new Task(4, "Task Overdue", false, "This task is overdue...", new Date("12.05.2015 14:00")),
+    new Task(5, "Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
+    new Task(6, "Task Two", true, null, new Date("12.05.2021 14:00")),
+    new Task(7, "Task Without Date", true, null, null),
+    new Task(8, "Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
+    new Task(9, "Task One", false, "This is the description for task one.", new Date("12.01.2021 14:00")),
+    new Task(10, "Task Two", true, null, new Date("12.05.2021 14:00")),
+    new Task(11, "Task Without Date", true, null, null),
+    new Task(12, "Task Overdue", true, "This task is overdue...", new Date("12.05.2015 14:00")),
 ];
 
 function isTaskOverdue(task) {
@@ -36,7 +37,7 @@ function getTaskDueDateTime(task) {
 function taskToDom(task) {
     var html = [];
 
-    html.push(`<div class='task-item'>`);
+    html.push(`<div class='task-item `, task.done ? `task-item_done` : ``, `' data-id='`, task.id, `'>`);
     html.push(`<a class='task-item__remove-icon' href="#">&#128465;</a>`);
 
     html.push(`<div class='task-item__header'>`);
@@ -60,8 +61,39 @@ function taskToDom(task) {
     return html.join("");
 }
 
-let taskListDom = document.getElementById("task-list");
+function refreshTasks() {
+    let taskListDom = document.getElementById("task-list");
+    let radioIncomplete = document.getElementById("filter-incomplete");
+    let radioAll = document.getElementById("filter-all");
 
-for (let task of tasks) {
-    taskListDom.innerHTML += taskToDom(task);
+    taskListDom.innerHTML = "";
+
+    for (let task of tasks) {
+        // if show all or show only incomplete
+        if (radioAll.checked || (radioIncomplete.checked && !task.done))
+            taskListDom.innerHTML += taskToDom(task);
+    }
 }
+
+function setFilterCheck(id) {
+    let radioIncomplete = document.getElementById("filter-incomplete");
+    let radioAll = document.getElementById("filter-all");
+
+    radioIncomplete.checked = false;
+    radioAll.checked = false;
+
+    document.getElementById(id).checked = true;
+}
+
+function filterAll(event) {
+    setFilterCheck("filter-all");
+    refreshTasks();
+}
+
+function filterIncomplete(event) {
+    setFilterCheck("filter-incomplete");
+    refreshTasks();
+}
+
+setFilterCheck("filter-all");
+filterAll(null); // force update
