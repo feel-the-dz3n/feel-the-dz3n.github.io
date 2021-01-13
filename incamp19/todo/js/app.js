72f6c378254c0ee4c5736ff70072c41422e5c58a
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-13 16:22:48 +0200

    Fix min datetime visuals

--- incamp19/todo/js/app.js
+++ incamp19/todo/js/app.js
@@ -109,6 +109,10 @@ function getTaskDueDateTime(task) {
     return getFormattedDate(date);
 }
 
+function isDateTimeCorrect(dateTime) {
+    return dateTime && !isNaN(dateTime) && dateTime > new Date(0);
+}
+
 function taskToDom(task) {
     var html = [];
 
@@ -121,7 +125,7 @@ function taskToDom(task) {
     html.push(`<h3 class="task-item__name `, task.done ? `task-item__name_done` : ``, `">`, task.title, "</h3>");
     html.push(`</div>`); // task-item__title-side-container
     html.push(`<div class='task-item__title-side-container'>`);
-    if (task.dueTime && !isNaN(task.dueTime)) {
+    if (isDateTimeCorrect(task.dueTime)) {
         html.push(`<div class="task-item__dueDateTime `, !task.done && isTaskOverdue(task) ? `task-item__dueDateTime_overdue` : ``, `">`, getTaskDueDateTime(task), "</div>");
     }
     html.push(`</div>`); // task-item__title-side-container
