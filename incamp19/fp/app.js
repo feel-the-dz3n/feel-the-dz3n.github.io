382fcfb1e132a662aae18187360b30279845002e
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-14 10:36:31 +0200

    Flow for maxBlackSquareArea & redRectsPerimeter

--- incamp19/fp/app.js
+++ incamp19/fp/app.js
@@ -1,16 +1,71 @@
 class Rectangle {
-    constructor(color) {
+    constructor(color, a, b, c, d) {
         this.color = color;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
     }
 }
 
-function hasColorTest() {
-    let hasColor = color => rectangle => rectangle.color == color;
-    let isRed = hasColor('red');
+let hasColor = color => rectangle => rectangle.color == color;
+let isRed = hasColor('red');
+let isBlack = hasColor('black');
 
+function hasColorTest() {
     let figures = [new Rectangle('red'), new Rectangle("#00FF00")];
     console.log('filter(isRed)', figures.filter(isRed));
     console.log('filter(hasColor("#00FF00"))', figures.filter(hasColor("#00FF00")));
 }
 
+let isSquare = rect => rect.a == rect.b && rect.c == rect.d;
+const and = (pA, pB) => item => pA(item) && pB(item); 
+let calcArea = rect => rect.a * rect.a;
+let calcAreas = rects => rects.map(calcArea);
+let collectBlackSquares = rects => rects.filter(and(isBlack, isSquare));
+
+function maxBlackSquareAreaTest() {
+    let figures = [
+        new Rectangle('black', 2, 3, 4, 5), // not a square
+        new Rectangle('black', 5, 5, 5, 5), // 25
+        new Rectangle('black', 6, 6, 6, 6), // 36
+        new Rectangle('black', 7, 7, 7, 7), // 49, expected result
+        new Rectangle('red', 8, 8, 8, 8) // 64, not black
+    ];
+    let findMaxBlackSquareArea = _.flow(
+        collectBlackSquares,
+        calcAreas,
+        _.max);
+    let maxBlackSquareArea = findMaxBlackSquareArea(figures);
+
+    console.log("maxBlackSquareArea", maxBlackSquareArea);
+}
+
+let collectRedRects = rects => rects.filter(isRed);
+let calcPerimeter = rect => rect.a + rect.b + rect.d + rect.c;
+let calcPerimeters = rects => rects.map(calcPerimeter);
+
+function redRectsPerimeterTest() {
+    let figures = [
+        new Rectangle('black', 2, 3, 2, 3), // ignore
+        new Rectangle('red', 2, 3, 2, 3), // 10
+        new Rectangle('red', 5, 4, 5, 4), // 18
+        new Rectangle('red', 9, 1, 9, 1), // 20
+        new Rectangle('yellow', 5, 8, 5, 8) // ignore
+    ];
+
+    // expected result = 10 + 18 + 20 = 48
+    let findRedRectsPerimeter = _.flow(
+        collectRedRects,
+        calcPerimeters,
+        _.sum
+    );
+
+    let redRectsPerimeter = findRedRectsPerimeter(figures);
+
+    console.log("redRectsPerimeterTest", redRectsPerimeter);
+}
+
 hasColorTest();
+maxBlackSquareAreaTest();
+redRectsPerimeterTest();
